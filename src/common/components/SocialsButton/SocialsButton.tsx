b52726b4f5a24fb5a176a9bc89f357d647aadb8e
skranee <125743202+skranee@users.noreply.github.com>
2024-08-23 18:37:25 +0300

    socialsButtons

---
+++ src/common/components/SocialsButton/SocialsButton.tsx
@@ -0,0 +1,36 @@
+import { ButtonHTMLAttributes, ReactNode } from 'react';
+import styles from './SocialsButton.module.scss';
+import youtube from './Icons/youtube-logo.png';
+import tg from './Icons/telegram-logo.png';
+import x from './Icons/x-logo.png';
+
+interface ISocialsButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  buttonIcon?: string,
+  children: ReactNode
+}
+
+export const SocialsButton = (props: ISocialsButtonProps) => {
+  const { children, buttonIcon, ...restProps } = props;
+  const determineIcon = () => buttonIcon === 'yt' ? youtube : buttonIcon === 'tg' ? tg : x;
+
+  const convertToK = (subscribers: ReactNode) => {
+    if (typeof subscribers === 'number') {
+      return Math.round(subscribers / 1000).toString() + 'k';
+    }
+
+    return '...';
+  };
+
+  return (
+    <button
+      className={styles.socialsButton}
+      {...restProps}
+    >
+      <img className={ styles.socialsButton__icon } src={ determineIcon() } alt='socialsButtonIcon' />
+      <div className={ styles.subscribersContainer }>
+        <span className={ styles.socialsButton__subscribers }>{ convertToK(children) }</span>
+        <span>Subscribers</span>
+      </div>
+    </button>
+  );
+};
