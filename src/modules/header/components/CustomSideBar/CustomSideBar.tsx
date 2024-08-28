0f5a4b052fcb71456c09fb31c75f29a2840f15b2
Murz4 <cool.murz@gmail.com>
2024-08-28 12:36:46 +0200

    add socialNetworks with button

--- src/modules/header/components/CustomSideBar/CustomSideBar.tsx
+++ src/modules/header/components/CustomSideBar/CustomSideBar.tsx
@@ -2,10 +2,22 @@ import { ButtonHTMLAttributes } from 'react';
 
 import { CloseReactIcon } from '@common/icons/CloseReactIcon';
 import { motion } from 'framer-motion';
+import { CustomIconButton } from '@common/components/CustomIconButton/CustomIconButton';
+import { YouTubeReactIcon } from '@common/icons/YouTubeReactIcon';
+import { TelegramReactIcon } from '@common/icons/TelegramReactIcon';
+import { InstagramReactIcon } from '@common/icons/InstagramReactIcon';
+import { TwitterReactIcon } from '@common/icons/TwitterReactIcon';
 
 import styles from './customSideBar.module.scss';
+import { CustomButton } from '@common/components/CustomButton/CustomButton';
 
 const navListArray = [{ text: 'Об авторе' }, { text: 'Программа' }, { text: 'Тарифы' }];
+const iconsArray = [
+  { icon: <YouTubeReactIcon />, Link: 'https://www.youtube.com/' },
+  { icon: <TelegramReactIcon />, Link: 'https://desktop.telegram.org/' },
+  { icon: <TwitterReactIcon />, Link: 'https://x.com/?lang=ru' },
+  { icon: <InstagramReactIcon />, Link: 'https://www.instagram.com/' },
+];
 
 interface ICustomSideBarProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   isOpen: boolean;
@@ -33,6 +45,18 @@ export const CustomSideBar = ({ onClose, isOpen }: ICustomSideBarProps) => (
           ))}
         </ul>
       </nav>
+      <div className={styles.customSideBar__bottom}>
+        <div className={styles.customSideBar__socialNetworks}>
+          {iconsArray.map((item, index) => (
+            <a href={item.Link} key={index}>
+              <CustomIconButton icon={item.icon} />
+            </a>
+          ))}
+        </div>
+        <div className={styles.customSideBar__buttonWrapper}>
+          <CustomButton buttonType='secondary'>Записаться</CustomButton>
+        </div>
+      </div>
     </div>
   </motion.div>
 );
