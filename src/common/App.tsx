f4cc963df902bdb18d55d521b7198f778bf2a403
skranee <125743202+skranee@users.noreply.github.com>
2024-08-28 16:37:34 +0300

    hotfix

--- src/common/App.tsx
+++ src/common/App.tsx
@@ -1,13 +1,8 @@
-import { CustomButton } from '@common/components/CustomButton/CustomButton';
 
 const App = () => (
   <div>
     <h2>Привет</h2>
-    <div style={{ width: 250, marginLeft: 150 }}>
-      <CustomButton height={45} buttonType='fourth' >
-        Стать участником
-      </CustomButton>
-    </div>
+
   </div>
 );
 
