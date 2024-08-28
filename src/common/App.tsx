35af04684f836a1243c488f5fd93382b24450306
skranee <125743202+skranee@users.noreply.github.com>
2024-08-28 16:09:32 +0300

    button_fix

--- src/common/App.tsx
+++ src/common/App.tsx
@@ -1,7 +1,13 @@
+import { CustomButton } from '@common/components/CustomButton/CustomButton';
 
 const App = () => (
   <div>
     <h2>Привет</h2>
+    <div style={{ width: 250, marginLeft: 150 }}>
+      <CustomButton height={45} buttonType='fourth' >
+        Стать участником
+      </CustomButton>
+    </div>
   </div>
 );
 
