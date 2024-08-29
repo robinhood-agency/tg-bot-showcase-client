e920c035148b43174bede7dabfa01945d8b8b9c5
skranee <125743202+skranee@users.noreply.github.com>
2024-08-29 16:07:24 +0300

    done

--- public/Crabs.tsx
+++ public/Crabs.tsx
@@ -1,7 +1,7 @@
 
 export const Crabs = () => (
   <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
-       width="400px" height="400px" viewBox="0 0 500.000000 500.000000"
+       width="380px" height="380px" viewBox="0 0 500.000000 500.000000"
        preserveAspectRatio="xMidYMid meet">
 
     <g transform="translate(0.000000,500.000000) scale(0.100000,-0.100000)"
