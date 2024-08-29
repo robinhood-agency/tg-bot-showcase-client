1ca187a001343bcc3ec60c3ef3e693bab5e41ba1
skranee <125743202+skranee@users.noreply.github.com>
2024-08-29 19:51:53 +0300

    hotfix

--- src/common/App.tsx
+++ src/common/App.tsx
@@ -1,11 +1,6 @@
-import { CustomInput } from '@common/components/CustomInput/CustomInput';
-
 const App = () => (
   <div>
     <h2>Привет</h2>
-    <div style={{ position: 'relative', width: 300, marginLeft: 100, marginTop: 100 }}>
-      <CustomInput label='Email' />
-    </div>
   </div>
 );
 
