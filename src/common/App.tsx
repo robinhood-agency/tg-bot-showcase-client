8ad5896bc8f6924a25ad3fbd183f235a3609774f
skranee <125743202+skranee@users.noreply.github.com>
2024-08-30 15:15:02 +0300

    hotfix

--- src/common/App.tsx
+++ src/common/App.tsx
@@ -1,6 +1,9 @@
+import { CustomInput } from '@common/components/CustomInput/CustomInput';
+
 const App = () => (
   <div>
     <h2>Привет</h2>
+    <CustomInput label='Email' />
   </div>
 );
 
--- src/common/components/CustomInput/CustomInput.tsx
+++ src/common/components/CustomInput/CustomInput.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
+import { InputHTMLAttributes } from 'react';
 
 import styles from './CustomInput.module.scss';
 
-interface ICustomInputProps extends React.HTMLAttributes<HTMLInputElement> {
+interface ICustomInputProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string,
-  value?: string
 }
 
-export const CustomInput = (props: ICustomInputProps) => {
-  const { label, value, ...restProps } = props;
-
-  return (
+export const CustomInput = ({ label, ...props }: ICustomInputProps) => (
     <div className={styles.inputContainer}>
       <input
         className={styles.inputContainer__input}
         id={label}
-        value={value}
         placeholder={label}
-        {...restProps}
+        {...props}
       />
       <label htmlFor={label} className={styles.inputContainer__label}>{ label }</label>
     </div>
   );
-};
