e22fe02d35a891a2f528af90847ff68938f7bdf2
Murz4 <cool.murz@gmail.com>
2024-09-04 19:30:14 +0200

    suka

--- src/modules/header/components/CustomSideBar/CustomSideBar.tsx
+++ src/modules/header/components/CustomSideBar/CustomSideBar.tsx
@@ -26,45 +26,47 @@ interface ICustomSideBarProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 export const CustomSideBar = ({ onClose, isOpen }: ICustomSideBarProps) => (
-  <motion.div
-    className={styles.customSideBar}
-    initial={{ x: '100%' }}
-    animate={{ x: isOpen ? 0 : '100%' }}
-    exit={{ x: '100%' }}
-    transition={{ type: 'just' }}
-  >
-    <div className={styles.customSideBar__container}>
-      <button className={styles.customSideBar__buttonClose} onClick={onClose}>
-        <CloseReactIcon />
-      </button>
-      <nav>
-        <ul className={styles.customSideBar__list}>
-          {navListArray.map((item, index) => (
-            <li key={index}>
-              <p className={styles.customSideBar__itemList}>{item.text}</p>
-            </li>
-          ))}
-        </ul>
-      </nav>
-      <div className={styles.customSideBar__bottom}>
-        <div className={styles.customSideBar__socialNetworks}>
-          {iconsArray.map((item, index) => (
-            <a href={item.Link} key={index}>
-              <CustomIconButton icon={item.icon} />
-            </a>
-          ))}
-        </div>
-        <div className={styles.customSideBar__buttonWrapper}>
-          <div className={styles.customSideBar__secondaryButton}>
-            <CustomButton buttonType='secondary' height={51}>
-              Записаться
+  <div className={`${styles.container} ${isOpen ? styles.container_active : ''}`}>
+    <motion.div
+      className={styles.customSideBar}
+      initial={{ x: '100%' }}
+      animate={{ x: isOpen ? 0 : '100%' }}
+      exit={{ x: '100%' }}
+      transition={{ type: 'just' }}
+    >
+      <div className={styles.customSideBar__container}>
+        <button className={styles.customSideBar__buttonClose} onClick={onClose}>
+          <CloseReactIcon />
+        </button>
+        <nav>
+          <ul className={styles.customSideBar__list}>
+            {navListArray.map((item, index) => (
+              <li key={index}>
+                <p className={styles.customSideBar__itemList}>{item.text}</p>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div className={styles.customSideBar__bottom}>
+          <div className={styles.customSideBar__socialNetworks}>
+            {iconsArray.map((item, index) => (
+              <a href={item.Link} key={index}>
+                <CustomIconButton icon={item.icon} />
+              </a>
+            ))}
+          </div>
+          <div className={styles.customSideBar__buttonWrapper}>
+            <div className={styles.customSideBar__secondaryButton}>
+              <CustomButton buttonType='secondary' height={51}>
+                Записаться
+              </CustomButton>
+            </div>
+            <CustomButton buttonType='fourth' height={51}>
+              У меня есть вопрос
             </CustomButton>
           </div>
-          <CustomButton buttonType='fourth' height={51}>
-            У меня есть вопрос
-          </CustomButton>
         </div>
       </div>
-    </div>
-  </motion.div>
+    </motion.div>
+  </div>
 );
