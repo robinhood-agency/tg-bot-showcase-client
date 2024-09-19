a84adecd2475e95fe4680bc3055db2c849c08d53
Murz4 <cool.murz@gmail.com>
2024-09-19 20:53:24 +0200

    card animate fix

--- src/modules/warming/Card.tsx
+++ src/modules/warming/Card.tsx
@@ -20,7 +20,7 @@ export const Card = ({ card, index }: ICardProps) => (
   >
     <motion.div
       className={styles.container__cardUpperText}
-      initial={{ opacity: 0, x: 40 }}
+      initial={{ opacity: 0, x: '100%' }}
       whileInView={{ opacity: 1, x: 0 }}
       transition={{ duration: 0.5, delay: 0.1 }}
       viewport={{ once: true }}
