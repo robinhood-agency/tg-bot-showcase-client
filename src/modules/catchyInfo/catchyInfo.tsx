ea281dd615d413cab79b6ffa3229f88fed5ffe99
Murz4 <cool.murz@gmail.com>
2024-09-20 06:46:18 +0200

    hot fix

--- src/modules/catchyInfo/catchyInfo.tsx
+++ src/modules/catchyInfo/catchyInfo.tsx
@@ -54,7 +54,7 @@ export const CatchyInfo = () => (
           </motion.p>
           <motion.p
             className={styles.container__itemTitle}
-            initial={{ opacity: 0, y: 80 }}
+            initial={{ opacity: 0, y: 70 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.6, delay: 0.1 }}
             viewport={{ once: true }}
